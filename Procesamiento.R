e40119f61f0059de4c3fb2d32b62eb33c4ef3d1c
Lu1sR <luishrt1997@gmail.com>
2019-08-15 16:53:06 -0700

    Procesamiento de tweets

---
+++ Procesamiento.R
@@ -0,0 +1,37 @@
+library(tidyverse)
+library(tidytext)
+
+
+download.file("https://raw.githubusercontent.com/jboscomendoza/rpubs/master/sentimientos_afinn/lexico_afinn.en.es.csv","lexico_afinn.en.es.csv")
+afinn <- read.csv("lexico_afinn.en.es.csv", stringsAsFactors = F, fileEncoding = "latin1") %>% tbl_df()
+
+afinn <- afinn[-c(2475,2476),]
+
+
+tuits <- read_csv2(file.choose(), quote = "\"", col_names = FALSE) %>% tbl_df()
+
+names(tuits) <-c("tuits")
+
+tuits <- tibble::rowid_to_column(tuits, "ID")
+
+tuits$tuits <- gsub("#\\w+","",tuits$tuits)
+tuits$tuits <- gsub("@\\w+","",tuits$tuits)
+tuits$tuits <- gsub("[[:punct:]]","",tuits$tuits)
+tuits$tuits <- gsub("\\w*[0-9]+\\w*\\s*", "",tuits$tuits)
+
+tuits_afinn <- 
+  tuits %>%
+  unnest_tokens(input = "tuits", output = "Palabra") %>%
+  inner_join(afinn, ., by = "Palabra") %>%
+  mutate(Tipo = ifelse(Puntuacion > 0, "Positiva", "Negativa"))
+
+
+tuits <-
+  tuits_afinn %>%
+  group_by(ID) %>%
+  summarise(Puntuacion_tuit = mean(Puntuacion)) %>%
+  left_join(tuits, ., by = "ID")
+
+
+tuits[is.na(tuits)] <- 0
+tuits$Puntuacion_tuit <- round(tuits$Puntuacion_tuit)
