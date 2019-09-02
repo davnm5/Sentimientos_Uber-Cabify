4bbec43a66274916d4a7e352df12cfe4ebe2619a
davnm5 <josdanei@espol.edu.ec>
2019-09-02 10:08:52 -0500

    [FIX] fase1-Python

--- fase3-Javascript/app.js
+++ fase3-Javascript/app.js
@@ -17,7 +17,7 @@ router.get('/wordcloud/positivas',function(req,res){
 });
 });
 router.get('/mapas',function(req,res){
-  fs.readFile("../output/output.csv", {encoding: 'utf-8'}, function(err,data){
+  fs.readFile("../fase2-R/output/output.csv", {encoding: 'utf-8'}, function(err,data){
     if (!err) {
         res.send(data);    
     } else {
