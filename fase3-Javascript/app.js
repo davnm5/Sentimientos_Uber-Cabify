e3c033cc5b8e8a761898d2ffa22f1cc32bb7cf6e
davnm5 <josdanei@espol.edu.ec>
2019-09-03 16:31:59 -0500

    [ADD] Cards Uber/Cabify

--- fase3-Javascript/app.js
+++ fase3-Javascript/app.js
@@ -5,90 +5,125 @@ var router = express.Router();
 const bodyParser = require('body-parser');
 const fs = require('fs');
 
-
-
-router.get('/wordcloud/positivas',function(req,res){
-  fs.readFile("../fase2-R/output/wordcloud_positivas.txt", {encoding: 'utf-8'}, function(err,data){
-    if (!err) {
+router.get('/wordcloud/positivas/:app', function (req, res) {
+  console.log(req.params.app);
+  if (req.params.app == "uber") {
+    fs.readFile("../fase2-R/output/wordcloud_positivas_uber.txt", { encoding: 'utf-8' }, function (err, data) {
+      if (!err) {
         res.send(data);
-    } else {
+      } else {
         console.log(err);
-    }
-});
-});
-router.get('/mapas',function(req,res){
-  fs.readFile("../fase2-R/output/output.csv", {encoding: 'utf-8'}, function(err,data){
-    if (!err) {
-        res.send(data);    
-    } else {
+      }
+    });
+  }
+  if (req.params.app == "cabify") {
+    fs.readFile("../fase2-R/output/wordcloud_positivas_cabify.txt", { encoding: 'utf-8' }, function (err, data) {
+      if (!err) {
+        res.send(data);
+      } else {
         console.log(err);
-    }
-});
+      }
+    });
+  }
+
 });
 
-router.get('/api/radar/:polaridad',function(req,res){
-  fs.readFile("../fase2-R/output/output.csv", {encoding: 'utf-8'}, function(err,data){
+router.get('/wordcloud/negativas/:app', function (req, res) {
+  if (req.params.app == "uber") {
+    fs.readFile("../fase2-R/output/wordcloud_negativas_uber.txt", { encoding: 'utf-8' }, function (err, data) {
+      if (!err) {
+        res.send(data);
+      } else {
+        console.log(err);
+      }
+    });
+  }
+  if (req.params.app == "cabify") {
+    fs.readFile("../fase2-R/output/wordcloud_negativas_cabify.txt", { encoding: 'utf-8' }, function (err, data) {
+      if (!err) {
+        res.send(data);
+      } else {
+        console.log(err);
+      }
+    });
+  }
+});
+
+router.get('/mapas', function (req, res) {
+  fs.readFile("../fase2-R/output/output.csv", { encoding: 'utf-8' }, function (err, data) {
     if (!err) {
-      var lines=data.split("\n");
+      res.send(data);
+    } else {
+      console.log(err);
+    }
+  });
+});
+
+router.get('/api/radar/:polaridad', function (req, res) {
+  fs.readFile("../fase2-R/output/output.csv", { encoding: 'utf-8' }, function (err, data) {
+    if (!err) {
+      var lines = data.split("\n");
       var result = [];
-      var headers=lines[0].split(",");
-      
-      for(var i=2;i<lines.length-1;i++){
+      var headers = lines[0].split(",");
+
+      for (var i = 2; i < lines.length - 1; i++) {
         var obj = {};
-        var currentline=lines[i].split(",");
-       
-        for(var j=0;j<headers.length;j++){
-          if(headers[j].toLowerCase().trim() === req.params.polaridad){          
+        var currentline = lines[i].split(",");
+
+        for (var j = 0; j < headers.length; j++) {
+          if (headers[j].toLowerCase().trim() === req.params.polaridad) {
             obj[headers[j]] = currentline[j];
             obj[headers[0]] = currentline[0];
-            
+
             obj["app"] = currentline[4];
-          } 
+          }
         }
         result.push(obj);
       }
       res.send(JSON.stringify(result)); //JSON
     } else {
-        console.log(err);
+      console.log(err);
     }
-});
-});
-
-router.get('/wordcloud/negativas',function(req,res){
-  fs.readFile("../fase2-R/output/wordcloud_negativas.txt", {encoding: 'utf-8'}, function(err,data){
-    if (!err) {
-        res.send(data);
-    } else {
-        console.log(err);
-    }
-});
+  });
 });
 
-router.get('/wordcloud',function(req,res){
-  res.sendFile(path.join(__dirname+'/wordcloud.html'));
-});
-router.get('/mapa',function(req,res){
-  res.sendFile(path.join(__dirname+'/mapa.html'));
-});
-router.get('/radar',function(req,res){
-  res.sendFile(path.join(__dirname+'/radar.html'));
+
+
+router.get('/wordcloud_uber', function (req, res) {
+  res.sendFile(path.join(__dirname + '/wordcloud_uber.html'));
 });
 
-app.use('/css',express.static('css'));
-app.use('/js',express.static('js'));
-app.use('/',router);
+router.get('/wordcloud_cabify', function (req, res) {
+  res.sendFile(path.join(__dirname + '/wordcloud_cabify.html'));
+});
+
+router.get('/index', function (req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+router.get('/mapa', function (req, res) {
+  res.sendFile(path.join(__dirname + '/mapa.html'));
+});
+router.get('/radar', function (req, res) {
+  res.sendFile(path.join(__dirname + '/radar.html'));
+});
+
+app.use('/css', express.static('css'));
+app.use('/js', express.static('js'));
+app.use('/img', express.static('img'));
+app.use('/', router);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(function(req, res, next) {
-    res.status(404).send("page not found");
-    console.error(res.statusCode+"page not found");
-  
-  });
+app.use(function (req, res, next) {
+  res.status(404).send("page not found");
+  console.error(res.statusCode + "page not found");
+
+});
 
 const host = '0.0.0.0';
-const port = process.env.PORT || 3000;  
-  
-app.listen(port, host, function() {
+const port = process.env.PORT || 3000;
+
+app.listen(port, host, function () {
   console.log("Server started...");
 });
---
+++ fase3-Javascript/js/wordcloud_uber.js
@@ -0,0 +1,49 @@
+
+$( document ).ready(function() {
+am4core.useTheme(am4themes_moonrisekingdom);
+am4core.useTheme(am4themes_animated);
+
+var series1=crearChart("chart1");
+var request = new XMLHttpRequest();
+  request.open('GET', 'http://localhost:3000/wordcloud/positivas/uber', true)
+  request.onload = function() {
+     var data= this.response;
+  
+    if (request.status >= 200 && request.status < 400) {
+        series1.text = data;
+    } else {
+      console.log('error');
+    }
+  }
+  request.send();
+
+var series2=crearChart("chart2");
+  var request2 = new XMLHttpRequest();
+  request2.open('GET', 'http://localhost:3000/wordcloud/negativas/uber', true)
+  request2.onload = function() {
+     var data= this.response;
+  
+    if (request2.status >= 200 && request2.status < 400) {
+        series2.text = data;
+    } else {
+      console.log('error');
+    }
+  }
+  request2.send();
+});
+
+
+function crearChart(name){
+  var chart = am4core.create(name, am4plugins_wordCloud.WordCloud);
+  var series = chart.series.push(new am4plugins_wordCloud.WordCloudSeries());
+  
+  series.accuracy = 2;
+  series.step = 20;
+  series.rotationThreshold = 0.5;
+  series.maxCount = 300;
+  series.minWordLength = 1;
+  series.labels.template.tooltipText = "{word}: {value}";
+  series.fontFamily = "Courier New";
+  series.maxFontSize = am4core.percent(20);
+return series
+}
